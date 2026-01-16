e8ed62f4302541c2ad63c0572916e572238aaac8
Aaronabil <antomi.tomi1979@gmail.com>
2026-01-16 19:37:10 +0700

    feat: add currencies and transactions model

---
+++ app/Models/Transactions.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Transactions extends Model
+{
+    use HasFactory, SoftDeletes, HasUuids;
+
+    protected $fillable = [
+        'invoice_number',
+        'user_id',
+        'currency_id',
+        'financial_account_id',
+        'type',
+        'customer_name',
+        'rate',
+        'amount',
+        'total_idr',
+        'cost_of_good_sold',
+        'profit',
+        'notes',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function currency()
+    {
+        return $this->belongsTo(Currencies::class);
+    }
+
+    public function financialAccount()
+    {
+        return $this->belongsTo(FinancialAccount::class);
+    }
+}
