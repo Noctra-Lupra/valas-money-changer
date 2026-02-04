39891831897e5802a7268118fa39f45cb5dcb6b5
Aaronabil <antomi.tomi1979@gmail.com>
2026-02-04 14:10:46 +0700

    style: change name  to company name and add menu if user in mobile layout

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -6,9 +6,7 @@ export default function Guest({ children }: PropsWithChildren) {
     return (
         <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
             <div>
-                <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
-                </Link>
+               <h1 className="text-2xl font-bold">BERTHAUT MONEY CHANGER</h1>
             </div>
 
             <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800">
