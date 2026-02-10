6c8018fe144b40dfc21949d5dbeb47952496efaf
Aaronabil <antomi.tomi1979@gmail.com>
2026-02-10 14:22:32 +0700

    fix style: change name to company name and add menu if user in mobile layout

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -107,8 +107,8 @@ export default function AuthenticatedLayout({
                                 href={route('dashboard')}
                                 className="flex items-center gap-2 font-semibold"
                             >
-                                <Package className="h-6 w-6" />
-                                <span className="">Nama Perusahaan</span>
+                                {/* <Package className="h-6 w-6" /> */}
+                                <span className="">Berthaut Money Changer</span>
                             </Link>
                         </div>
                         <div className="flex-1 overflow-auto py-2">
@@ -120,7 +120,6 @@ export default function AuthenticatedLayout({
                                     <div className="space-y-1">
                                         {menus[role]?.map((menu) => {
                                             const Icon = menu.icon;
-
                                             return (
                                                 <NavLink
                                                     key={menu.route}
@@ -170,9 +169,9 @@ export default function AuthenticatedLayout({
                                     className="w-56"
                                 >
                                     <DropdownMenuLabel>
-                                        My Account
+                                        My Account, login as {user.role}
                                     </DropdownMenuLabel>
-                                    <DropdownMenuSeparator />
+                                    {/* <DropdownMenuSeparator />
                                     <DropdownMenuItem>
                                         <Link
                                             href={route('profile.edit')}
@@ -180,7 +179,7 @@ export default function AuthenticatedLayout({
                                         >
                                             Profile
                                         </Link>
-                                    </DropdownMenuItem>
+                                    </DropdownMenuItem> */}
                                     <DropdownMenuSeparator />
                                     <DropdownMenuItem variant="destructive">
                                         <Link
@@ -217,54 +216,29 @@ export default function AuthenticatedLayout({
                                 <nav className="grid gap-2 text-lg font-medium">
                                     <div className="mb-4 flex items-center gap-2 text-lg font-semibold">
                                         <Package className="h-6 w-6" />
-                                        <span>Nama Perusahaan</span>
+                                        <span>Berthaut Money Changer</span>
                                     </div>
                                     <div className="px-2 py-2">
                                         <h3 className="mb-2 text-xs font-semibold uppercase text-muted-foreground">
                                             General
                                         </h3>
                                         <div className="space-y-1">
-                                            <NavLink
-                                                href={route('dashboard')}
-                                                active={route().current(
-                                                    'dashboard',
-                                                )}
-                                                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                                            >
-                                                <LayoutGrid className="h-5 w-5" />
-                                                Dashboard
-                                            </NavLink>
-                                            <Link
-                                                href="#"
-                                                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                                            >
-                                                <ListTodo className="h-5 w-5" />
-                                                Tasks
-                                            </Link>
-                                            <Link
-                                                href="#"
-                                                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                                            >
-                                                <Package className="h-5 w-5" />
-                                                Apps
-                                            </Link>
-                                            <Link
-                                                href="#"
-                                                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                                            >
-                                                <MessageSquare className="h-5 w-5" />
-                                                Chats
-                                                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                                                    3
-                                                </Badge>
-                                            </Link>
-                                            <Link
-                                                href="#"
-                                                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                                            >
-                                                <Users className="h-5 w-5" />
-                                                Users
-                                            </Link>
+                                            {menus[role]?.map((menu) => {
+                                                const Icon = menu.icon;
+                                                return (
+                                                    <NavLink
+                                                        key={menu.route}
+                                                        href={route(menu.route)}
+                                                        active={route().current(
+                                                            menu.route,
+                                                        )}
+                                                        className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                                                    >
+                                                        <Icon className="h-4 w-4" />
+                                                        {menu.label}
+                                                    </NavLink>
+                                                );
+                                            })}
                                         </div>
                                     </div>
                                 </nav>
