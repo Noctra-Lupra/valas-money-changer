aa3d57c897c8de8cac713ff652f439181acc7fca
Aaronabil <antomi.tomi1979@gmail.com>
2026-02-25 14:14:36 +0700

    Fix api/index.php amnesia error

--- api/index.php
+++ api/index.php
@@ -1,36 +1,16 @@
 <?php
 
-// 1. Buat lorong rahasia di /tmp untuk Laravel
-$tmpDirs = [
-    '/tmp/storage/framework/views',
-    '/tmp/storage/framework/cache/data',
-    '/tmp/storage/framework/sessions',
-    '/tmp/storage/logs',
-    '/tmp/bootstrap/cache'
-];
+// 1. Buat folder sementara HANYA untuk views
+$viewDir = '/tmp/storage/framework/views';
 
-foreach ($tmpDirs as $dir) {
-    if (!file_exists($dir)) {
-        mkdir($dir, 0777, true);
-    }
+if (!file_exists($viewDir)) {
+    mkdir($viewDir, 0777, true);
 }
 
-// 2. Paksa Laravel menulis SEMUA file cache sistem ke /tmp
-// Inilah yang menyembuhkan error "Target class [view] does not exist"
-$envOverrides = [
-    'APP_SERVICES_CACHE' => '/tmp/bootstrap/cache/services.php',
-    'APP_PACKAGES_CACHE' => '/tmp/bootstrap/cache/packages.php',
-    'APP_CONFIG_CACHE'   => '/tmp/bootstrap/cache/config.php',
-    'APP_ROUTES_CACHE'   => '/tmp/bootstrap/cache/routes.php',
-    'APP_EVENTS_CACHE'   => '/tmp/bootstrap/cache/events.php',
-    'VIEW_COMPILED_PATH' => '/tmp/storage/framework/views',
-];
+// 2. Arahkan compiler view ke folder /tmp tersebut
+putenv("VIEW_COMPILED_PATH={$viewDir}");
+$_ENV['VIEW_COMPILED_PATH'] = $viewDir;
+$_SERVER['VIEW_COMPILED_PATH'] = $viewDir;
 
-foreach ($envOverrides as $key => $value) {
-    putenv("$key=$value");
-    $_ENV[$key] = $value;
-    $_SERVER[$key] = $value;
-}
-
-// 3. Nyalakan mesin utama Laravel
+// 3. Jalankan aplikasi Laravel
 require __DIR__ . '/../public/index.php';
