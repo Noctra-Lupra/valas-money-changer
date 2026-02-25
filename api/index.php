02ed692466cd7d6f7eb31f266a35dc7894a8e166
Aaronabil <antomi.tomi1979@gmail.com>
2026-02-25 14:07:23 +0700

    feat: Create `api/index.php` to configure Laravel to use `/tmp` for cache and session storage, resolving the 'Target class [view] does not exist' error.

--- api/index.php
+++ api/index.php
@@ -1,16 +1,12 @@
 <?php
 
-// JURUS X-RAY: Memaksa Laravel merespons dalam bentuk JSON
-// Ini akan mem-bypass sistem 'view' yang sedang error
-$_SERVER['HTTP_ACCEPT'] = 'application/json';
-
+// 1. Buat lorong rahasia di /tmp untuk Laravel
 $tmpDirs = [
     '/tmp/storage/framework/views',
-    '/tmp/storage/framework/cache',
     '/tmp/storage/framework/cache/data',
     '/tmp/storage/framework/sessions',
     '/tmp/storage/logs',
-    '/tmp/bootstrap/cache',
+    '/tmp/bootstrap/cache'
 ];
 
 foreach ($tmpDirs as $dir) {
@@ -19,11 +15,22 @@ foreach ($tmpDirs as $dir) {
     }
 }
 
-// Paksa semua path penyimpanan ke /tmp
-putenv('APP_STORAGE=/tmp/storage');
-$_ENV['APP_STORAGE'] = '/tmp/storage';
+// 2. Paksa Laravel menulis SEMUA file cache sistem ke /tmp
+// Inilah yang menyembuhkan error "Target class [view] does not exist"
+$envOverrides = [
+    'APP_SERVICES_CACHE' => '/tmp/bootstrap/cache/services.php',
+    'APP_PACKAGES_CACHE' => '/tmp/bootstrap/cache/packages.php',
+    'APP_CONFIG_CACHE'   => '/tmp/bootstrap/cache/config.php',
+    'APP_ROUTES_CACHE'   => '/tmp/bootstrap/cache/routes.php',
+    'APP_EVENTS_CACHE'   => '/tmp/bootstrap/cache/events.php',
+    'VIEW_COMPILED_PATH' => '/tmp/storage/framework/views',
+];
 
-putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+foreach ($envOverrides as $key => $value) {
+    putenv("$key=$value");
+    $_ENV[$key] = $value;
+    $_SERVER[$key] = $value;
+}
 
+// 3. Nyalakan mesin utama Laravel
 require __DIR__ . '/../public/index.php';
