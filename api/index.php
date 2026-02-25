5a839bfd3f37eab725ef1f426029c1d549aad154
Aaronabil <antomi.tomi1979@gmail.com>
2026-02-25 13:52:10 +0700

    feat: Add `api/index.php` to configure Laravel temporary directories for Vercel deployment.

--- api/index.php
+++ api/index.php
@@ -1,2 +1,23 @@
 <?php
+
+// 1. Bikin folder sementara di sistem Vercel agar Laravel bisa napas
+$tmpDirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($tmpDirs as $dir) {
+    if (!file_exists($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
+// 2. Paksa Laravel untuk menggunakan folder /tmp tersebut
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+
+// 3. Jalankan aplikasi Laravel
 require __DIR__ . '/../public/index.php';
