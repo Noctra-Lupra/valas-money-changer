77443c8b40b59fde50e69e20889dfcb829e29551
Aaronabil <antomi.tomi1979@gmail.com>
2026-02-25 14:21:51 +0700

    feat: Configure Laravel temporary storage and view paths to use `/tmp` in `api/index.php`.

--- api/index.php
+++ api/index.php
@@ -1,16 +1,31 @@
 <?php
 
-// 1. Buat folder sementara HANYA untuk views
-$viewDir = '/tmp/storage/framework/views';
+// 1. Buat folder sementara yang komprehensif untuk Laravel
+$tmpStorage = '/tmp/storage';
 
-if (!file_exists($viewDir)) {
-    mkdir($viewDir, 0777, true);
+$dirs = [
+    "$tmpStorage/app/public",
+    "$tmpStorage/framework/cache/data",
+    "$tmpStorage/framework/sessions",
+    "$tmpStorage/framework/testing",
+    "$tmpStorage/framework/views",
+    "$tmpStorage/logs"
+];
+
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
 }
 
-// 2. Arahkan compiler view ke folder /tmp tersebut
-putenv("VIEW_COMPILED_PATH={$viewDir}");
-$_ENV['VIEW_COMPILED_PATH'] = $viewDir;
-$_SERVER['VIEW_COMPILED_PATH'] = $viewDir;
+// 2. Paksa Laravel menggunakan /tmp untuk SEMUA storage dan view
+putenv("APP_STORAGE=$tmpStorage");
+$_ENV['APP_STORAGE'] = $tmpStorage;
+$_SERVER['APP_STORAGE'] = $tmpStorage;
+
+putenv("VIEW_COMPILED_PATH=$tmpStorage/framework/views");
+$_ENV['VIEW_COMPILED_PATH'] = "$tmpStorage/framework/views";
+$_SERVER['VIEW_COMPILED_PATH'] = "$tmpStorage/framework/views";
 
 // 3. Jalankan aplikasi Laravel
 require __DIR__ . '/../public/index.php';
